d909372a89b07aa311ad4d3168a3e31e13ab0b2d
ove <isiful.ovi@gmail.com>
2023-03-07 03:31:28 +0600

    Update data Loader

--- src/main/java/com/sio/poveacl/acl/configuration/ResourceUrl.java
+++ src/main/java/com/sio/poveacl/acl/configuration/ResourceUrl.java
@@ -1,5 +1,6 @@
 package com.sio.poveacl.acl.configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ResourceUrl {
@@ -17,16 +18,50 @@ public class ResourceUrl {
     public static final String FEATURE_DASHBOARD = "/api/features/dashboard";
 
 
+    public static final String CREATE_DESIGNATION = "/api/designations/create-designations";
+    public static final String UPDATE_DESIGNATION = "/api/designations/update-designation/{id}";
+    public static final String SHOW_DESIGNATION = "/api/designations/show-designation/{id}";
+    public static final String DESIGNATION_LIST = "/api/designations/list";
+    public static final String DESIGNATION_DASHBOARD = "/api/designations/dashboard";
+
+
     public static final AuthRecord CREATE_USER_RECORD = new AuthRecord("CREATE_USER", CREATE_USER_ENDPOINT, "Create User");
     public static final AuthRecord USER_LIST_RECORD = new AuthRecord("USER_LIST", USER_LIST, "User List");
     public static final AuthRecord USER_DASHBOARD_RECORD = new AuthRecord("USER_DASHBOARD", USER_DASHBOARD, "Dashboard");
+    public static final AuthRecord UPDATE_USER_RECORD = new AuthRecord("UPDATE_USER", UPDATE_USER, "Update User");
+    public static final AuthRecord SHOW_USER_RECORD = new AuthRecord("SHOW_USER", SHOW_USER, "Show User");
+
+    public static final AuthRecord CREATE_FEATURE_RECORD = new AuthRecord("CREATE_FEATURE", CREATE_FEATURE, "Create Feature");
+    public static final AuthRecord UPDATE_FEATURE_RECORD = new AuthRecord("UPDATE_FEATURE", UPDATE_FEATURE, "Update Feature");
+    public static final AuthRecord SHOW_FEATURE_RECORD = new AuthRecord("SHOW_FEATURE", SHOW_FEATURE, "Show Feature");
+    public static final AuthRecord FEATURE_LIST_RECORD = new AuthRecord("FEATURE_LIST", FEATURE_LIST, "List Features");
+    public static final AuthRecord FEATURE_DASHBOARD_RECORD = new AuthRecord("FEATURE_DASHBOARD", FEATURE_DASHBOARD, "Feature Dashboard");
+
+    public static final AuthRecord CREATE_DESIGNATION_RECORD = new AuthRecord("CREATE_DESIGNATION", CREATE_DESIGNATION, "Create Designation");
+    public static final AuthRecord UPDATE_DESIGNATION_RECORD = new AuthRecord("UPDATE_DESIGNATION", UPDATE_DESIGNATION, "Update Designation");
+    public static final AuthRecord SHOW_DESIGNATION_RECORD = new AuthRecord("SHOW_DESIGNATION", SHOW_DESIGNATION, "Show Designation");
+    public static final AuthRecord DESIGNATION_LIST_RECORD = new AuthRecord("DESIGNATION_LIST", DESIGNATION_LIST, "List Designations");
+    public static final AuthRecord DESIGNATION_DASHBOARD_RECORD = new AuthRecord("DESIGNATION_DASHBOARD", DESIGNATION_DASHBOARD, "Designation Dashboard");
+
 
 
     public static List<AuthRecord> getUserFeatureList() {
         return List.of(
                 CREATE_USER_RECORD,
+                UPDATE_USER_RECORD,
+                SHOW_USER_RECORD,
                 USER_LIST_RECORD,
-                USER_DASHBOARD_RECORD
+                USER_DASHBOARD_RECORD,
+                CREATE_FEATURE_RECORD,
+                UPDATE_FEATURE_RECORD,
+                SHOW_FEATURE_RECORD,
+                FEATURE_LIST_RECORD,
+                FEATURE_DASHBOARD_RECORD,
+                CREATE_DESIGNATION_RECORD,
+                UPDATE_DESIGNATION_RECORD,
+                SHOW_DESIGNATION_RECORD,
+                DESIGNATION_LIST_RECORD,
+                DESIGNATION_DASHBOARD_RECORD
         );
     }
 }
--- src/main/java/com/sio/poveacl/acl/seed/DataLoader.java
+++ src/main/java/com/sio/poveacl/acl/seed/DataLoader.java
@@ -1,5 +1,6 @@
 package com.sio.poveacl.acl.seed;
 
+import com.sio.poveacl.acl.configuration.ResourceUrl;
 import com.sio.poveacl.acl.domain.AppUser;
 import com.sio.poveacl.acl.domain.Feature;
 import com.sio.poveacl.acl.domain.Role;
@@ -12,9 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.List;
 
 import static com.sio.poveacl.acl.configuration.ResourceUrl.CREATE_USER_RECORD;
-import static com.sio.poveacl.acl.configuration.ResourceUrl.USER_LIST_RECORD;
 
 @Component
 @RequiredArgsConstructor
@@ -28,66 +30,67 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        if (userRepository.findAll().size() == 0) {
-            AppUser adminUser = new AppUser();
-            adminUser.setUsername("admin");
-            adminUser.setEmail("admin@gmail.com");
-            adminUser.setPassword(passwordEncoder.encode("1234"));
-            adminUser.setFullName("Admin");
-            adminUser.setDateCreated(OffsetDateTime.now());
-            adminUser = userRepository.save(adminUser);
+        userRepository.deleteAll();
+        roleRepository.deleteAll();
+        featureRepository.deleteAll();
 
-            AppUser staffUser = new AppUser();
-            staffUser.setUsername("staff");
-            staffUser.setEmail("staff@gmail.com");
-            staffUser.setPassword(passwordEncoder.encode("1234"));
-            staffUser.setFullName("Staff");
-            staffUser.setDateCreated(OffsetDateTime.now());
-            staffUser = userRepository.save(staffUser);
 
-            Role adminRole = new Role();
-            adminRole.setName("Super Admin");
-            adminRole.setDescription("Super Admin role");
-            adminRole.setTitle("SUPER ADMIN FOR APPLICATION");
-            adminRole = roleRepository.save(adminRole);
-
-            Role staffRole = new Role();
-            staffRole.setName("Staff User");
-            staffRole.setDescription(" Staff role");
-            staffRole.setTitle(" Staff FOR APPLICATION");
-            staffRole = roleRepository.save(staffRole);
-
+        List<Feature> list = ResourceUrl.getUserFeatureList().stream().map(item -> {
             Feature createUserFeature = new Feature();
-            createUserFeature.setName(CREATE_USER_RECORD.name());
-            createUserFeature.setDescription("create user");
-            createUserFeature.setUrl(CREATE_USER_RECORD.url());
-            createUserFeature = featureRepository.save(createUserFeature);
+            createUserFeature.setName(item.name());
+            createUserFeature.setDescription(item.name());
+            createUserFeature.setUrl(item.url());
+            return createUserFeature;
 
-            Feature userListFeature = new Feature();
-            userListFeature.setName(USER_LIST_RECORD.name());
-            userListFeature.setDescription("user list");
-            userListFeature.setUrl(USER_LIST_RECORD.url());
-            userListFeature = featureRepository.save(userListFeature);
+        }).toList();
 
-            adminRole.getFeatures().add(createUserFeature);
-            adminRole.getFeatures().add(userListFeature);
-            adminRole = roleRepository.save(adminRole);
-            adminUser.getRoles().add(adminRole);
-            adminUser.setAccessGiven(true);
-            userRepository.save(adminUser);
+        featureRepository.saveAll(list);
 
-            staffRole.getFeatures().add(userListFeature);
-            staffRole = roleRepository.save(staffRole);
-            staffUser.getRoles().add(staffRole);
-            staffUser.setAccessGiven(true);
 
-            userRepository.save(staffUser);
-        }
+        AppUser adminUser = new AppUser();
+        adminUser.setUsername("admin");
+        adminUser.setEmail("admin@gmail.com");
+        adminUser.setPassword(passwordEncoder.encode("1234"));
+        adminUser.setFullName("Admin");
+        adminUser.setDateCreated(OffsetDateTime.now());
+        adminUser = userRepository.save(adminUser);
+
+        AppUser staffUser = new AppUser();
+        staffUser.setUsername("staff");
+        staffUser.setEmail("staff@gmail.com");
+        staffUser.setPassword(passwordEncoder.encode("1234"));
+        staffUser.setFullName("Staff");
+        staffUser.setDateCreated(OffsetDateTime.now());
+        staffUser = userRepository.save(staffUser);
+
+        Role adminRole = new Role();
+        adminRole.setName("Super Admin");
+        adminRole.setDescription("Super Admin role");
+        adminRole.setTitle("SUPER ADMIN FOR APPLICATION");
+        adminRole = roleRepository.save(adminRole);
+
+        Role staffRole = new Role();
+        staffRole.setName("Staff User");
+        staffRole.setDescription(" Staff role");
+        staffRole.setTitle(" Staff FOR APPLICATION");
+        staffRole = roleRepository.save(staffRole);
+
+        adminRole.setFeatures(new HashSet<>(featureRepository.findAll()));
+        adminRole = roleRepository.save(adminRole);
+        adminUser.getRoles().add(adminRole);
+        adminUser.setAccessGiven(true);
+        userRepository.save(adminUser);
+
+        staffRole = roleRepository.save(staffRole);
+        staffUser.getRoles().add(staffRole);
+        staffUser.setAccessGiven(true);
+
+        userRepository.save(staffUser);
+
 
        /* userRepository.deleteAll();
         roleRepository.deleteAll();
         featureRepository.deleteAll();*/
-
 
 
     }
